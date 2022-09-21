2b6f9615401c292e4ca3f75cf40f592ae6923b0e
vkp3012 <vivekpatel301295@gmail.com>
2022-09-21 10:05:09 +0530

    competed display comments part

---
+++ reels/components/DisplayComments.js
@@ -0,0 +1,49 @@
+import { Avatar } from "@mui/material";
+import { doc, getDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import CircularProgress from "@mui/material/CircularProgress";
+
+function DisplayComments({ postData }) {
+    const [allComments, setAllComments] = useState(null);
+    
+    useEffect(() => {
+        getComments();
+    }, [postData]);
+
+    function getComments() {
+        let tempArray = [];
+        postData.comments.map(async (commentId) => {
+            const docSnap = await getDoc(doc(db, "comments", commentId));
+            tempArray.push(docSnap.data());
+            setAllComments(tempArray);
+        });
+    }
+    return (
+        <div>
+            {allComments == null ? (
+                <CircularProgress color="success" />
+            ) : (
+                <>
+                    {allComments.map((commentObj) => {
+                        return (
+                            <div style={{ display: "flex" }}>
+                                <Avatar src={commentObj.userDP} />
+                                <p>
+                                    <span style={{ fontWeight: "bold" }}>
+                                        &nbsp;
+                                        {commentObj.userName}
+                                    </span>
+                                    &nbsp;&nbsp;
+                                    {commentObj.text}
+                                </p>
+                            </div>
+                        );
+                    })}
+                </>
+            )}
+        </div>
+    );
+}
+
+export default DisplayComments;
