6dad20eccf7a730cb5c2b9211d3cd3210c0d7d09
vkp3012 <vivekpatel301295@gmail.com>
2023-01-17 19:03:09 +0530

    header css is done

--- portfolio_app/src/components/header.js
+++ portfolio_app/src/components/header.js
@@ -1,66 +1,58 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
-// import avatar from './images/avatar.png'
 import './styles/header.css'
 import Form from './form'
 import { Link } from "react-router-dom"
 import name from './images/Vivek Patel-logos_black.png'
 function Header() {
-
   return (
     <div>
           <header>
               <div class="content">
-                      <h2>Hello I'm <h1>Vivek Kumar Patel.</h1></h2><br/>
-                      <p class="content1">A Web Developer activelly looking <br/> for a new possition. I love to explore <br/>new technologies and always try to<br/> enhance my skills through develop<br/> some projects.</p>
+                      <h2>Hello I'm, <h1 class="name">Vivek Kumar Patel.</h1></h2><br/>
+                      <p class="content1">A Web Developer activelly looking for a new possition. I love to explore <br/>new technologies and always try to enhance my skills through develop<br/> some projects.</p>
               </div>
-              {/* <div class='avatar' >
-                  <img src={avatar} class='avatar1'></img>
-              </div> */}
           </header>
           <div class="container " style={{margin:"0",top:0}}>
             <div class="row align-items-center">
-              <div class="col-lg-7 pr-lg-5  mb-lg-0">
+              <div class="col">
                 <img src={name} alt="placeholder" class="img-fluid" />
               </div>
-            <div class="col-lg-5">
-              <div class="section-heading">
-                <h2>
-                  About <strong>Me</strong>
-                </h2>
+              <div class="col-lg-6 mb-5 ml-5">
+                <div class="section-heading">
+                  <h2>
+                    About <strong>Me</strong>
+                  </h2>
+                </div>
+                <p class="mb">
+                  I am a passionate Full Stake Software developer and I interested
+                  to study updated technologies and tackle complex problems. I
+                  completed my Bachelors in Electronics and Communication Engineering. My
+                  technical and management skills which will use to support the
+                  growth of the organization as well as myself.
+                </p>
+                <p class="btn-1">
+                  <a
+                    href='./from.jsx'
+                    class="btn btn-primary px-4 py-2 btn-sm smoothscroll"
+                  >
+                    <Link to="/form" style={{textDecoration:"none",color:"White",display:"inline"}}>
+                          Hire Me
+                        <span>{Form}</span>
+                      </Link>
+                  </a>
+                  <a
+                    href="https://drive.google.com/file/d/1DWEG4xb_DakS6EXtLTZVN86jylctulBR/view"
+                    class="btn btn-primary px-4 py-2 btn-sm"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Download CV
+                  </a>
+                </p>
               </div>
-              <p class="mb-5">
-                I am a passionate Full Stake Software developer and I interested
-                to study updated technologies and tackle complex problems. I
-                completed my Bachelors in Electronics and Communication Engineering. My
-                technical and management skills which will use to support the
-                growth of the organization as well as myself.
-              </p>
-              <p>
-                <a
-                  href='./about.js'
-                  class="btn btn-primary px-4 py-2 btn-sm smoothscroll"
-                >
-                  <Link to="/form" style={{textDecoration:"none",color:"White",display:"inline"}}>
-                        Hire Me
-                      <span>{Form}</span>
-                    </Link>
-                </a>
-                <a
-                  href="https://drive.google.com/file/d/1DWEG4xb_DakS6EXtLTZVN86jylctulBR/view"
-                  class="btn btn-secondary px-4 py-2 btn-sm"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Download CV
-                </a>
-              </p>
             </div>
           </div>
-      </div>
-      {/* <div class="form-section">
-          <Form />
-      </div> */}
     </div>
   )
 }
