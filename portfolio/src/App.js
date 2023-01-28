b3533cb874bc44d4957ef44b62017f9f4297e36a
vkp3012 <vivekpatel301295@gmail.com>
2023-01-28 13:47:22 +0530

    create portfolio app using react

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './components/home/navbar'
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
