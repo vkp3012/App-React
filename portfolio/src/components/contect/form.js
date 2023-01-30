2681c8a2b03b2f3448145ce14484ea231ebb5942
vkp3012 <vivekpatel301295@gmail.com>
2023-01-30 13:24:51 +0530

    form part is done

--- portfolio/src/components/contect/form.js
+++ portfolio/src/components/contect/form.js
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react'
 import emailjs from "@emailjs/browser";
 import {service_id,temp,public_key}from '../../secrects'
+import './form.css';
 
 const Form = () => {
   const form = useRef();
@@ -17,48 +18,69 @@ const Form = () => {
         )
         .then(
             (result) => {
-            console.log(result.text);
-            console.log("message sent");
+              alert("send Email")
+              console.log(result.text);
+              console.log("message sent");
             },
             (error) => {
-            console.log(error.text);
+              alert("check your email.")
+              console.log(error.text);
             }
         );
     };
 
   return (
-    <div>
-      <div>
-        <div>
+    <div class="contect">
+        <div class="contect-form">
           <h1>Wanna Start Work With Me?</h1>
-          <form ref={form} onSubmit={sendEmail}>
-              <label>Name</label>
-              <input type="text" name="user_name" />
-              <label>Email</label>
-              <input type="email" name="user_email" />
-              <label>Message</label>
-              <textarea name="message" />
+          <hr/>
+          <form ref={form} onSubmit={sendEmail} class="form">
+              <label>Name &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </label>
+                <input 
+                  type="text" 
+                  name="user_name" 
+                />
+              <label>Email &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </label>
+                <input 
+                  type="email" 
+                  name="user_email" 
+                  required
+                  placeholder="username@beststartupever.com"
+                  pattern=".+@gmail\outlook\beststartupever\.com"
+                  title="Please provide only a Best Startup Ever corporate email address"   
+                />
+              <label>Message &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </label>
+                <textarea name="message" />
               <input type="submit" value="Send" />
           </form>
         </div>
-      </div>
-      <div>
-      <h3 class="mb">My Contact Details</h3>
-        <ul class="site-contact-details mb-9" >
+      <div class="contect-detalies">
+      <h3 class="mb-2">My Contact Details</h3>
+      <hr/>
+        <ul class="site-contact-details" style={{color:"black",padding:"20px 0"}}>
             <li>
-                <span class="text-uppercase" style={{color:"black"}}>Email</span><br/>
-                    <span class="email" style={{color:"black"}}> vivekece19@gmail.com</span>
+              <span class="email"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;&nbsp; vivekece19@gmail.com</span>
             </li>
             <li>
-                <span class="text-uppercase" style={{color:"black"}}>Phone</span><br/>
-                <span style={{color:"black"}}>+91, 9935162491</span>
+                <span><i class="fa fa-phone" aria-hidden="true"></i>&nbsp;&nbsp; +91, 9935162491</span>
             </li>
-            <li><span class="text-uppercase" style={{color:"black"}}>Address</span><br/>
-            <span style={{color:"black"}}>
-                Churamanpur,Post Bhulanpur PAC <br/> Varanasi Uttar Pradesh <br/> India, 221108
-            </span>
+            <li>
+              <span>
+                <i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;&nbsp; Churamanpur,Post Bhulanpur PAC <br/> Varanasi Uttar Pradesh <br/> India, 221108
+              </span>
             </li>
         </ul>
+
+      <h3 class="mb-2">Social Media</h3>
+      <hr/>
+      <p class="social-media" style={{padding:"20px 10px"}}>
+        <p class="logo-hover"><i class="fa fa-instagram" aria-hidden="true"></i>&nbsp; Instagram</p>
+        <p class="logo-hover"><i class="fa fa-linkedin-square" aria-hidden="true"></i>&nbsp;&nbsp;LinkedIn</p>
+        <p class="logo-hover"><i class="fa fa-github" aria-hidden="true"></i>&nbsp;&nbsp; GitHub</p>
+      </p>
       </div>
     </div>
   )
