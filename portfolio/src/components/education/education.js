797e51d2089f33bef35dcc1997b523adc62a39d7
vkp3012 <vivekpatel301295@gmail.com>
2023-01-31 16:12:41 +0530

    eduction part media query is done

--- portfolio/src/components/education/education.js
+++ portfolio/src/components/education/education.js
@@ -5,14 +5,14 @@ function education() {
   return (
     <div class="resume">
       <section class="section-1">
-        <div class="resume-section">
-            <h2><strong>Resume</strong></h2>
+        <div class="mt-5">
+            <h2 class="p-1"><strong>Resume</strong></h2>
             <hr style={{width:"50%",padding:"0 50%",alignItems:"center", display:"flex",justifyContent:"center"}}/>
         </div>
-        <div class="resume-section-1">
-          <div class="eduction-section">
+        <div class="resume-0 d-flex flex-row col-12 p-2">
+          <div class="resume-1 col-6">
             <h2><strong>Eduction</strong></h2>
-            <div class="edu-detailes">
+            <div class="p-3 bg-secondary">
               <span>
                 <i class="fa fa-calendar" aria-hidden="true"></i> August 2015 - 2019 May
               </span>
@@ -27,9 +27,9 @@ function education() {
               </h3>
             </div>
           </div>
-          <div class="experience-section">
+          <div class=" resume-1 col-6 mx-1">
             <h2><strong>Experience</strong></h2>
-            <div class="exp-detailes">
+            <div class="p-3 bg-secondary">
               <span>
                 <i class="fa fa-calendar" aria-hidden="true"></i> January 2022 - July 2022
               </span>
@@ -46,42 +46,43 @@ function education() {
           </div>
         </div>
       </section>
-      <section class="section-2">
-        <div class="skill-section">
+      <section class="section-2 row m-2">
+        <div class="skill-section col-2">
           <h2>My<strong> Skills</strong></h2>
           <hr/>
         </div>
-            <div class="skill-detailes">
-              <div class="container-1">
-                <p>HTML</p>
-                <div class="container">
-                    <div class="skill percentage html">90%</div>
+            <div class="col-10 p-4">
+              <div class="d-flex col-10">
+                <h5 >HTML</h5>
+                <div class="progress m-1 col-12" role="progressbar" aria-label="Basic example" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">  
+                  <div class="progress-bar" style={{width: "95%"}}>Html</div>
                 </div>
               </div>
-              <div class="container-1">
-                <p>CSS</p>
-                <div class="container">
-                    <div class="skill percentage css">80%</div>
+              <div class="d-flex col-12">
+                <h5 >CSS</h5>
+                <div class="progress m-1 col-10" role="progressbar" aria-label="Basic example" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">  
+                  <div class="progress-bar" style={{width: "90%"}}>CSS</div>
                 </div>
               </div>
-              <div class="container-1">
-                <p>Node JS</p>
-                <div class="container">
-                    <div class="skill percentage node">70%</div>
+              <div class="d-flex col-12">
+                <h5 >Node JS</h5>
+                <div class="progress m-1 col-10" role="progressbar" aria-label="Basic example" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">  
+                  <div class="progress-bar" style={{width: "70%"}}>Node JS</div>
                 </div>
               </div>
-              <div class="container-1">
-                <p>React</p>
-                <div class="container">
-                    <div class="skill percentage react">70%</div>
+              <div class="d-flex col-12">
+                <h5 >React</h5>
+                <div class="progress m-1 col-10" role="progressbar" aria-label="Basic example" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">  
+                  <div class="progress-bar" style={{width: "70%"}}>React</div>
                 </div>
               </div>
-              <div class="container-1">
-                <p>BootStrap</p>
-                <div class="container">
-                    <div class="skill percentage bs">70%</div>
+              <div class="d-flex col-12">
+                <h5 >BootStrap</h5>
+                <div class="progress m-1 col-10" role="progressbar" aria-label="Basic example" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">  
+                  <div class="progress-bar" style={{width: "75%"}}>BootStrap</div>
                 </div>
               </div>
+              
             </div>
       </section>
     </div>
