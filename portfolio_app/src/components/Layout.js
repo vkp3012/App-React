a5da2db066987f228a13516a363d937b0e3d2ed9
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-02-24 14:58:10 +0530

    build is done

--- portfolio_app/src/components/Layout.js
+++ portfolio_app/src/components/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from './Header';
+// import Header from './Header';
 import Footer from './Footer';
 
 const Layout = () => {
     return (
         <>
-            <Header/>
+            {/* <Header/> */}
             <Outlet/>
             <Footer />
         </>
